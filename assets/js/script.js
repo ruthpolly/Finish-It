368f5233439099bc50ba1f0544fada1e9bad7b03
ruthpolly <>
2023-05-01 22:10:52 +0000

    Add function to display phrase to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,53 +2,53 @@
 let phrases = [
     {
         phrase: "Stop messing around and acting the ______!",
-        answers: ['maggot', 'prankster', 'shoe-lace', 'baby'],
-        answer: 'maggot',
+        options: ['shoe lace', 'prankster', 'maggot', 'baby'],
+        answer: 2,
     },
     {
         phrase: "I passed Mary on the road, she was driving like she was ______.",
-        answers: ['late for mass', 'going to work', 'cycling', 'walking'],
-        answer: 'late for mass',
+        options: ['late for mass', 'going to work', 'cycling', 'walking'],
+        answer: 0,
     },
     {
         phrase: "Stop crying now or I will give you ______!",
-        answers: ['something to cry about', 'a present', 'icecream', 'a punsihment'],
-        answer: 'something to cry about'
+        options: ['a punishment', 'a present', 'icecream', 'something to cry about'],
+        answer: 3
     },
     {
         phrase: "Siobhan had a baby so we are on our way to ______.",
-        answers: ['visit her', 'wet the babys head', 'go shopping', 'eat dinner'],
-        answer: 'wet the babys head'
+        options: ['visit her', 'wet the babys head', 'go shopping', 'eat dinner'],
+        answer: 1
     },
     {
         phrase: "Dad is home, did you ______?",
-        answers: ['turn off the immersion', 'wash', 'eat', 'clean up'],
-        answer: 'turn off the immersion'
+        options: ['turn off the immersion', 'wash', 'eat', 'clean up'],
+        answer: 0
     },
     {
         phrase: "Were you _______? Close the door after you.",
-        answers: ['born in a field', 'cold outside', 'waiting long', 'on the way'],
-        answer: 'born in a field'
+        options: ['waiting long', 'cold outside', 'born in a field', 'on the way'],
+        answer: 2
     },
     {
         phrase: "Ah the car is _____ I will have to get a new one",
-        answers: ['banjaxed', 'broken', 'crashed', 'dopey'],
-        answer: 'banjaxed'
+        options: ['broken', 'banjaxed', 'crashed', 'dopey'],
+        answer: 1
     },
     {
         phrase: "Yeah I am _____ with the new job",
-        answers: ['delira', 'happy', 'in love', 'busy'],
-        answer: 'delira'
+        options: ['happy', 'delira', 'in love', 'busy'],
+        answer: 1
     },
     {
         phrase: "Sure come over to my _____ for some tae",
-        answers: ['gaff', 'house', 'place', 'bag'],
-        answer: 'gaff'
+        options: ['bag', 'house', 'place', 'gaff'],
+        answer: 3
     },
     {
         phrase: "Oh I am _____ for you!",
-        answers: ['scarlet', 'embarrassed', 'happy', 'wagon'],
-        answer: 'scarlet'
+        options: ['scarlet', 'embarrassed', 'happy', 'wagon'],
+        answer: 0
     },
 ];
 
@@ -56,13 +56,27 @@ let phrases = [
 let homeArea = document.getElementsByClassName("home-area");
 let quizArea = document.getElementsByClassName("quiz-area");
 let resultsArea = document.getElementsByClassName("results-area");
+let quizPhrase = document.getElementById("phrase");
+let answerButtons = document.getElementById('answer-buttons');
+let randomPhrase, currentPhrase;
 
 // function to start quiz
 function startQuiz() {
     homeArea[0].classList.add("hide");
     quizArea[0].classList.remove("hide");
+    randomPhrase = phrases.sort(() => Math.random() - .5);
+    currentPhrase = 0;
+    nextPhrase();
 }
 
-console.log('connected');
+function nextPhrase() {
+    showPhrase(randomPhrase[currentPhrase]);
+}
 
+function showPhrase(phrase) {
+    quizPhrase.innerText = phrase.phrase;
+    phrase.options.forEach(answer => {
+        
+    })
+}
 // function to check the answer
